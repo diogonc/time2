a198d4577b78f3a8b35661ecc3917b08795126d6
Yuri Claure <yuriclaure@gmail.com>
2016-12-16 09:32:53 -0300

    Adicionando bulma e FA

--- TimeDois/App_Start/RouteConfig.cs
+++ TimeDois/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TimeDois
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Eventos", action = "Listar", id = UrlParameter.Optional }
             );
         }
     }
