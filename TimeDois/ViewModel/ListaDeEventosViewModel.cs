df6a29ae6cbd07cbb53ffab5784378c500abd56a
Diogo <diogonc@gmail.com>
2016-12-16 17:46:12 -0300

    nao sei

--- TimeDois/ViewModel/ListaDeEventosViewModel.cs
+++ TimeDois/ViewModel/ListaDeEventosViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using TimeDois.Controllers;
 using TimeDois.Models;
 
@@ -7,5 +8,22 @@ namespace TimeDois.ViewModel
     public class ListaDeEventosViewModel
     {
         public IEnumerable<Evento> Eventos;
+
+        public string CssDoStatusDoInteresse(string login, ICollection<Participacao> participantes)
+        {
+            if (participantes.Any(participacao => participacao.Usuario.Login == login && participacao.Status == StatusDaParticipacao.Aprovado))
+            {
+                return "status-aprovado";
+            }
+            if (participantes.Any(participacao => participacao.Usuario.Login == login && participacao.Status == StatusDaParticipacao.EmAnalise))
+            {
+                return "status-em-analise";
+            }
+            if (participantes.Any(participacao => participacao.Usuario.Login == login && participacao.Status == StatusDaParticipacao.Reprovado))
+            {
+                return "status-reprovado";
+            }
+            return "";
+        }
     }
 }
