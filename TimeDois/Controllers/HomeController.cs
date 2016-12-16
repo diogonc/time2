0b01ca8beccc0d74e009d4c32574ba5932ab9989
Yuri Claure <yuriclaure@gmail.com>
2016-12-16 09:21:57 -0300

    Foi

--- TimeDois/Controllers/HomeController.cs
+++ TimeDois/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ namespace TimeDois.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
             return View();
